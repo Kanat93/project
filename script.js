2835edf0055bf9c69b5f7db7beb4273e070ec3a6
Kanat Koimaganbetov <kanat93kn@gmail.com>
2022-04-10 13:49:09 +0600

    add modify js file

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     generes: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    generes: [],
+    genres: [],
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+const question1 = prompt('Один из последних просмотренных фильмов?', '');
+const question2 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+personalMovieDB.movies[question1] = question2;
 
 console.log(personalMovieDB);
-
 
 
