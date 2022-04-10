6b9feb07db8be180c94e96264c3764038ba52ef7
Kanat <kanat93kn@gmail.com>
2022-04-10 14:23:40 +0600

    add swich

--- script.js
+++ script.js
@@ -1,40 +1,13 @@
 "use strict";
 
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+const num = 15;
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     generes: [],
-//     privat: false
-// };
-
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//       b = prompt('На сколько оцените его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов?', ''),
-//       d = prompt('На сколько оцените его?', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-// console.log(personalMovieDB);
-
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const question1 = prompt('Один из последних просмотренных фильмов?', '');
-const question2 = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[question1] = question2;
-
-console.log(personalMovieDB);
-
+switch (num) {
+    case 49:
+        console.log('Error');
+        break;
+    case 15:
+        console.log('Ok');
+        break;
+}
 
